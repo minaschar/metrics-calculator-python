6354cd5c157b69f5d545959f654f8b9ee01ae021
MINAS\User <ics20072@uom.edu.gr>
2022-08-23 12:32:22 +0300

    Create SizeCategory to keep values for quality metrics that are related with size

---
+++ app/src/size_category.py
@@ -0,0 +1,87 @@
+
+class SizeCategory:
+
+    WMC = 0
+    NOCC = 0
+    NOM = 0
+    LOC = 0
+    SIZE2 = 0
+    NDC = 0
+    DSC = 0
+    NOP = 0
+    CIS = 0
+    NPM = 0
+    NOC = 0
+    WAC = 0
+
+    def getWMC(self):
+        return self.WMC
+
+    def getNOCC(self):
+        return self.NOCC
+
+    def getNOM(self):
+        return self.NOM
+
+    def getLOC(self):
+        return self.LOC
+
+    def getSIZE2(self):
+        return self.SIZE2
+
+    def getNDC(self):
+        return self.NDC
+
+    def getDSC(self):
+        return self.DSC
+
+    def getNOP(self):
+        return self.NOP
+
+    def getCIS(self):
+        return self.CIS
+
+    def getNPM(self):
+        return self.NPM
+
+    def getNOC(self):
+        return self.NOC
+
+    def getWAC(self):
+        return self.WAC
+
+    def setWMC(self, value):
+        self.WMC = value
+
+    def setNOCC(self, value):
+        self.NOCC = value
+
+    def setNOM(self, value):
+        self.NOM = value
+
+    def setLOC(self, value):
+        self.LOC = value
+
+    def setSIZE2(self, value):
+        self.SIZE2 = value
+
+    def setNDC(self, value):
+        self.NDC = value
+
+    def setDSC(self, value):
+        self.DSC = value
+
+    def setNOP(self, value):
+        self.NOP = value
+
+    def setCIS(self, value):
+        self.CIS = value
+
+    def setNPM(self, value):
+        self.NPM = value
+
+    def setNOC(self, value):
+        self.NOC = value
+
+    def setWAC(self, value):
+        self.WAC = value
