03576b65ffd84f9a58a37059a391f054cbfe9414
Panagiotis Machairas <ics20044@uom.edu.gr>
2022-09-02 10:57:19 +0300

    Implementation of NOC

--- app/src/main.py
+++ app/src/main.py
@@ -4,7 +4,7 @@ from generate_ast import ASTGenerator
 from visitor import *
 
 # put here a project path to test the code
-test_root_folder_path = "C:/Users/User/Desktop/UoM/Parsers/ProjectForTesting"
+test_root_folder_path = "C:/Users/John/Desktop/game-master-t"
 
 test_project_name = "Game"
 project = Project(test_root_folder_path, test_project_name)
@@ -16,10 +16,19 @@ ASTGenerator(project).start_parsing()
 for python_file in project.get_files():
     visit_Class().visit_ClassDef(python_file.get_generated_ast(), python_file)
 
+classes = []
+
+
 # Calculate Metrics for each class
 for python_file in project.get_files():
     for classObj in python_file.getFileClasses():
         MetricsCalculator(classObj)
+        classes.append(classes)
+
+
+sc = SizeCategory()
+
+MetricsCalculator.calcNOC(classes, sc)
 
 
 # Testing - print Data
--- app/src/metrics_calculator.py
+++ app/src/metrics_calculator.py
@@ -1,13 +1,17 @@
 from classDecl import Class
+from size_category import SizeCategory
 
 
 # In this class exists methods that calculates metrics for each class
 class MetricsCalculator:
 
+    classes = []
+
     def __init__(self, classObj: Class):
         self.classObj = classObj
         self.calcWMC1()
         self.calcNOM()
+        # self.calcNOC()
 
     # Count the number of methods for each class in the project
     def calcNOM(self):
@@ -16,3 +20,7 @@ class MetricsCalculator:
     # Need changes
     def calcWMC1(self):
         self.classObj.getSizeCategoryMetrics().setWMC1(len(self.classObj.get_methods()))
+
+    @staticmethod
+    def calcNOC(classes, sc):
+        sc.setNOC(len(classes))
