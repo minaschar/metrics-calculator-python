2b1051bb13560baed5427ad3dfb547c87e1b8ac2
MINAS\User <ics20072@uom.edu.gr>
2022-11-28 23:28:46 +0200

    Fix bugs. Now we have in mind and the case that we call a method with this way: Classname().methodCall()

--- app/src/visitors/visitor.py
+++ app/src/visitors/visitor.py
@@ -85,12 +85,10 @@ class LCOM_Visitor(ast.NodeVisitor):
         self.fields.clear()  # Clear the set to can be used from the next method
 
     def visit_Attribute(self, node):
-        # Instance attributes
-        if (node.value.id == "self"):
-            self.fields.add("self." + node.attr)
-        # Class attributes that declared inside a method
-        elif (node.value.id == self.classObj.get_name()):
-            self.fields.add(self.classObj.get_name() + "." + node.attr)
+        if (isinstance(node.value, ast.Name)):
+            self.fields.add(node.value.id + "." + node.attr)
+        elif(isinstance(node.value, ast.Call)):
+            self.fields.add(node.value.func.id + "." + node.attr)
 
 
 class MethodsCalled_Visitor(ast.NodeVisitor):
@@ -100,18 +98,23 @@ class MethodsCalled_Visitor(ast.NodeVisitor):
         self.called = set()
 
     def visit_ClassDef(self, node):
-        self.generic_visit(node)
+        for child in ast.walk(node):
+            if (isinstance(child, ast.Call)):
+                self.visit_Call(child)
         return self.called
 
     def visit_Call(self, node):
+        self.generic_visit(node)
 
-        if (isinstance(node.func, ast.Attribute)):
-            if (node.func.value.id != "self"):
-                methodCalled = node.func.value.id + "." + node.func.attr
-                for pythonFile in self.classObj.getPyFileObj().getProject().get_files():
-                    for classObj in pythonFile.getFileClasses():
-                        if (methodCalled and node.func.attr in classObj.get_methods().keys()):
-                            self.called.add(methodCalled)
+    def visit_Attribute(self, node):
+        if (isinstance(node.value, ast.Name)):
+            methodCalled = node.value.id + "." + node.attr
+            for pythonFile in self.classObj.getPyFileObj().getProject().get_files():
+                for classObj in pythonFile.getFileClasses():
+                    if (methodCalled and node.attr in classObj.get_methods().keys()):
+                        self.called.add(methodCalled)
+        else:
+            self.generic_visit(node)
 
 
 class Hierarchy_Visitor(ast.NodeVisitor):
@@ -145,13 +148,19 @@ class MPC_Visitor(ast.NodeVisitor):
 
     def visit_FunctionDef(self, node):
 
-        for child in node.body:
-            self.generic_visit(child)
+        for child in ast.walk(node):
+            if (isinstance(child, ast.Call)):
+                self.visit_Call(child)
 
     def visit_Call(self, node):
+        self.generic_visit(node)
 
-        if(isinstance(node.func, ast.Attribute) and node.func.value.id != "self"):
-            self.messages = self.messages + 1
+    def visit_Attribute(self, node):
+        if (isinstance(node.value, ast.Name)):
+            if (node.value.id != "self"):
+                self.messages += 1
+        else:
+            self.generic_visit(node)
 
 
 class CBO_Visitor(ast.NodeVisitor):
@@ -168,13 +177,12 @@ class CBO_Visitor(ast.NodeVisitor):
         return self.elements
 
     def visit_FunctionDef(self, node):
-
-        for child in node.body:
-            self.generic_visit(child)
+        self.generic_visit(node)
 
     def visit_Attribute(self, node):
-        if(node.value.id != "self"):
-            self.elements[node.value.id] = node.attr
+        if (isinstance(node.value, ast.Name)):
+            if(node.value.id != "self"):
+                self.elements[node.value.id] = node.attr
 
 
 class CC_Visitor(ast.NodeVisitor):
