d0db847fdff2b38c7920e5d82d17a08e86617a7c
MINAS\User <ics20072@uom.edu.gr>
2022-11-29 20:44:06 +0200

    Fix the way that we traversing the ast in init

--- app/src/visitors/visitor.py
+++ app/src/visitors/visitor.py
@@ -18,7 +18,7 @@ class Init_Visitor(ast.NodeVisitor):
     # Visit the node of the whole .py file
     def visit_Module(self, node):
         # We need for loop because in one .py file can be more than one classes
-        for child in node.body:
+        for child in ast.walk(node):
             # We want to start analyzing only for classes and no for non oop code
             if (isinstance(child, ast.ClassDef)):
                 self.visit_ClassDef(child)
@@ -33,28 +33,29 @@ class Init_Visitor(ast.NodeVisitor):
             # We will visit the whole node of a method
             if (isinstance(child, ast.FunctionDef)):
                 self.visit_FunctionDef(child)
-            elif (isinstance(child, ast.ClassDef)):
-                self.visit_ClassDef(child)
-            else:
-                # In else, we are outside of the methods, so we will visit this part
-                AttrVisitor(classObj).visit(child)
+            # In else, we are outside of the methods, so we will visit this part
+            elif (isinstance(child, ast.Assign)):
+                print(ast.dump(child))
+                AttrVisitor(classObj).generic_visit(child)
 
     # Visit the node of a method in a class
     def visit_FunctionDef(self, node):
         # Get method arguments
         arguments = [arg.arg for arg in node.args.args]
         self.currClass.add_method(node.name, arguments)
+        # continue searching deeper in ast nodes
         self.generic_visit(node)
 
     # Visitor to get instance attributes and class attributes that declared in method's body!
     def visit_Attribute(self, node):
         if (isinstance(node.ctx, ast.Store)):
-            # Instance attributes
-            if (node.value.id == "self"):
-                self.currClass.add_field("self." + node.attr)
-            # Class attributes that declared inside a method
-            elif (node.value.id == self.currClass.get_name()):
-                self.currClass.add_field(self.currClass.get_name() + "." + node.attr)
+            if (isinstance(node.value, ast.Name)):
+                # Instance attributes
+                if (node.value.id == "self"):
+                    self.currClass.add_field("self." + node.attr)
+                # Class attributes that declared inside a method
+                elif (node.value.id == self.currClass.get_name()):
+                    self.currClass.add_field(self.currClass.get_name() + "." + node.attr)
 
 
 class AttrVisitor(ast.NodeVisitor):
